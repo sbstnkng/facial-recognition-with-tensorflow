4464c17685daa26d942acc671d7c20d473fdaeb6
sbstnkng <koenigse@gmx.de>
2022-04-25 07:44:00 +0200

    Create application header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/header';
 
-function App() {
+const App: React.FC = () => {
+  const [isPlaying, setPlaying] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        isPlaying={isPlaying}
+        onButtonClick={() => setPlaying(!isPlaying)}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Button from '@mui/material/Button';
+import PlayArrow from '@mui/icons-material/PlayArrow';
+import Stop from '@mui/icons-material/Stop';
+
+interface Props {
+  isPlaying: boolean;
+  onButtonClick: () => void;
+}
+
+const Header: React.FC<Props> = ({ isPlaying, onButtonClick }) => {
+  return (
+    <AppBar position="static">
+      <Container>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            Facial Recognition with Tensorflow
+          </Typography>
+          <Button
+            variant={isPlaying ? 'contained' : 'outlined'}
+            startIcon={isPlaying ? <Stop /> : <PlayArrow />}
+            color={isPlaying ? 'secondary' : 'inherit'}
+            sx={{ marginRight: '1rem', width: '6rem' }}
+            onClick={onButtonClick}
+          >
+            {isPlaying ? 'Stop' : 'Start'}
+          </Button>
+          <IconButton
+            size="large"
+            aria-label="settings"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            color="inherit"
+          >
+            <SettingsIcon />
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
