791aca4fc7029d62683466b2061499e08ceedea0
sbstnkng <koenigse@gmx.de>
2022-05-07 12:23:01 +0200

    Separate styles

--- src/components/video/VideoPanel.tsx
+++ src/components/video/VideoPanel.tsx
@@ -3,19 +3,27 @@ import { useSelector } from 'react-redux';
 import Container from '@mui/material/Container';
 import Webcam from 'react-webcam';
 import { RootState } from '../../redux/store';
+import { SxProps } from '@mui/material';
 
 const VideoPanel: React.FC = () => {
   const webcamRef = useRef(null);
   const isEnabled = useSelector((state: RootState) => state.video.isEnabled);
 
+  const containerStyles: SxProps = {
+    mt: 2,
+    display: 'flex',
+    justifyContent: 'center',
+  };
+  const videStyles: React.CSSProperties = {
+    height: '80vh',
+    maxHeight: '80vh',
+    maxWidth: '100vw',
+    borderRadius: '.5rem',
+  };
+
   return (
-    <Container sx={{ mt: 2, display: 'flex', justifyContent: 'center' }}>
-      {isEnabled && (
-        <Webcam
-          ref={webcamRef}
-          style={{ height: '80vh', maxHeight: '80vh', maxWidth: '100vw' }}
-        />
-      )}
+    <Container sx={containerStyles}>
+      {isEnabled && <Webcam ref={webcamRef} style={videStyles} />}
     </Container>
   );
 };
